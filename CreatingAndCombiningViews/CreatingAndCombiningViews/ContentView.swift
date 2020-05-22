741b21eeaa15b39f10cdfbfb5c50f656854b0aba
Said Ozcan <said.ozcan@booking.com>
2020-05-22 10:31:10 +0200

    initial commit for CreatingAndCombiningViews

---
+++ CreatingAndCombiningViews/CreatingAndCombiningViews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CreatingAndCombiningViews
+//
+//  Created by Said Ozcan on 22/05/2020.
+//  Copyright © 2020 Said Ozcan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
